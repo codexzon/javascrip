4f4df101fb24500e7f4dd0ef0816186ff207912e
Satyam Pal <em.satyam@gmail.com>
2025-05-03 11:01:59 +0530

    Starting JS #03052025

---
+++ 2_cont_let_var.js
@@ -0,0 +1,14 @@
+// console.log("welcome to codexzon")
+// console.log("welcome to us90 ")
+
+const studentId = 1234
+
+studentName = "Shivam";
+studentEmail = "shivam@gmail.com";
+studentContact = "5994994959";
+
+// studentId = 12345
+
+console.log(studentId);
+
+// console.table([studentName, studentEmail, studentContact]);098
---
+++ 3_let.js
@@ -0,0 +1,6 @@
+const  studentName = "Ankit";
+
+
+const studentName = "ankita";
+
+console.log(studentName);
