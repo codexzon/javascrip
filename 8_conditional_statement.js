2d2bc0f1ecd45a8042e37863b1d536da28692446
Satyam Pal <em.satyam@gmail.com>
2025-05-03 19:20:09 +0530

    conditional statement

---
+++ 8_conditional_statement.js
@@ -0,0 +1,51 @@
+// alert("welcome to our website");
+
+// let userName = prompt("Write your name");
+
+// console.log(userName);
+
+// let personAge = 1;
+
+// if (personAge >= 18) {
+//     console.log("you are eligible for licence");
+// }
+// if (personAge < 21) {
+//     console.log("you are not  eligible for licence");
+// }
+
+// if (personAge >= 18) {
+//     console.log("you are eligible for licence");
+// }else{
+//     console.log("not eligible");
+// }
+
+// let studentMark = 99;
+
+// if(studentMark>90) {
+//     console.log("10 GPA");
+// }
+// else if(studentMark>80) {
+//     console.log("9 GPA");
+// }else{
+//     console.log(" you fail");
+// }
+
+// let personAge = 1;
+
+// if (personAge >= 18) {
+//     console.log("you are eligible for licence");
+// }
+// if (personAge < 21) {
+//     console.log("you are not  eligible for licence");
+
+// }
+
+
+ let personAge = 17;
+
+if (personAge >= 18) {
+    console.log("you are eligible for licence");
+}
+if (personAge < 21) {
+    console.log("you are not  eligible for licence");
+}
