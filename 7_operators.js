e4472f0d587324eaf3a23910ef87d9c1283c8b7f
Satyam Pal <em.satyam@gmail.com>
2025-05-03 17:25:08 +0530

    operands and operator

---
+++ 7_operators.js
@@ -0,0 +1,54 @@
+
+
+// ------Arithmetic operators -----
+
+// console.log(2+6);
+// console.log(2-6);
+// console.log(2*6);
+// console.log(2/6);
+
+// ------modulas -------(reminder called)
+
+    // console.log(6%5);
+
+
+// ---- exponential -------
+ 
+    // console.log(3**2);
+
+// ----uninary ----
+
+// let a = 5;
+
+// console.log(a++); will show 5;
+// console.log(++a);  will show 6;
+
+// console.log(a--); will show 5;
+// console.log(a--);
+
+
+// ---- assignment ---
+
+// let a = 5;
+
+// a+=5;
+
+// let a = 5;
+
+// a/=25;
+// console.log(a);
+
+// -----comperision ----
+
+// let a = 3;
+//  let b = 4;
+
+//  console.log(a==b);
+
+
+
+let a = 3;
+ let b = 3;
+
+ console.log(a>=b);
+
