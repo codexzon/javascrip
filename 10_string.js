680d90c3f00155d86d90e5e08fba44d3baf649e6
Satyam Pal <em.satyam@gmail.com>
2025-05-04 10:19:27 +0530

    string js

---
+++ 10_string.js
@@ -0,0 +1,39 @@
+
+
+// // use opening and closing quotes are same
+
+// console.log(str.length);
+
+
+// console.log(str[2]);
+
+// console.log("wellcome to",str, "hey this is", str1, "thanks for listen");
+
+// console.log(`wellcome to ${str} hey this is ${str1} thanks for listen`);
+
+
+//scape charactor
+
+// console.log("well \n done");// in next line
+
+// console.log("well \t done"); // create space for a tab space
+
+// console.log("well \n done");  // delete last keyword  - wel done
+// let str = "codexz on";
+
+// let str1 = 'Satyam';
+
+// let abcd = str.toUpperCase();
+
+// console.log(abcd);
+// console.log(str);
+
+
+
+let str4 = "Hello Everyone";
+
+console.log(str4.slice(6,14));
+
+console.log(str4.replace("Hello","Hey"));
+
+console.log(str4.replace("o","i"));
