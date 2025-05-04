256b6c75790b0a5b6b60c09d399b88e02ffd8e85
Satyam Pal <em.satyam@gmail.com>
2025-05-04 20:09:58 +0530

    Math and Number

---
+++ 11_math_number.js
@@ -0,0 +1,36 @@
+// let bankBalance = new Number(95100.2345);
+
+// console.log(typeof bankBalance);
+
+// console.log(bankBalance.toString().length); --It goes to converted in string easy make changes;
+
+// console.log(bankBalance.toFixed(2));  it will count or after decimal values --95100.23;
+
+// console.log(bankBalance.toPrecision(2)); it will start counting exponential vale --- 9.5e+4
+
+// console.log(bankBalance.toLocaleString('en-IN'));
+
+
+// MATH KIBERARRY ;
+
+// console.log(Math.abs(-50)); 50
+
+// console.log(Math.round(5.3)); 5
+
+// console.log(Math.ceil(5.3)); 6
+
+// console.log(Math.floor(5.9)); 5
+
+// console.log(Math.sqrt(16)); 4
+
+// console.log(Math.min(5,38,93,29)); 5
+
+// console.log(Math.max(5,38,93,29)); 93
+
+// console.log(Math.random()*10); 
+
+let min = 20;
+let max = 200;
+console.log(Math.random()*(min, max)); 
+
+
